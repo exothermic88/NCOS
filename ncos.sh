577eba15c1378b5e260fe99dad827137cdfcf510
exothermic88 <92395170+exothermic88@users.noreply.github.com>
2021-11-08 23:33:06 -0500

    Update ncos.sh

--- ncos.sh
+++ ncos.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
     
+    chmod +x 0-preinstall.sh
+    chmod +x 1-setup.sh
+    chmod +x 2-user.sh
+    chmod +x 3-post-setup.sh
     bash 0-preinstall.sh
     arch-chroot /mnt /root/NCOS/1-setup.sh
     source /mnt/root/NCOS/install.conf
