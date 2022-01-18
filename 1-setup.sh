9389d935089d7943ec58a1886e94ecc55d65aa7c
exothermic88 <92395170+exothermic88@users.noreply.github.com>
2022-01-17 22:57:49 -0500

    Update 1-setup.sh

--- 1-setup.sh
+++ 1-setup.sh
@@ -226,14 +226,14 @@ case "$proc_type" in
 esac	
 
 # Graphics Drivers find and install
-if lspci | grep -E "NVIDIA|GeForce"; then
-    pacman -S nvidia --noconfirm --needed
-	nvidia-xconfig
-elif lspci | grep -E "Radeon"; then
-    pacman -S xf86-video-amdgpu --noconfirm --needed
-elif lspci | grep -E "Integrated Graphics Controller"; then
-    pacman -S libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils --needed --noconfirm
-fi
+#if lspci | grep -E "NVIDIA|GeForce"; then
+#    pacman -S nvidia --noconfirm --needed
+#	nvidia-xconfig
+#elif lspci | grep -E "Radeon"; then
+#    pacman -S xf86-video-amdgpu --noconfirm --needed
+#elif lspci | grep -E "Integrated Graphics Controller"; then
+#    pacman -S libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils --needed --noconfirm
+#fi
 
 echo -e "\nDone!\n"
 if ! source install.conf; then
