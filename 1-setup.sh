1d2fdf18b5c305ff50f7d55ae65a9fa7e9d1cc51
exothermic88 <92395170+exothermic88@users.noreply.github.com>
2022-01-20 20:33:38 -0500

    removed graphics drivers for testing

--- 1-setup.sh
+++ 1-setup.sh
@@ -99,14 +99,14 @@ echo -ne "
 "
 # Graphics Drivers find and install
 gpu_type=$(lspci)
-if grep -E "NVIDIA|GeForce" <<< ${gpu_type}; then
-    pacman -S nvidia --noconfirm --needed
-	nvidia-xconfig
-elif lspci | grep 'VGA' | grep -E "Radeon|AMD"; then
-    pacman -S xf86-video-amdgpu --noconfirm --needed
-elif grep -E "Integrated Graphics Controller" <<< ${gpu_type}; then
-    pacman -S libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa --needed --noconfirm
-elif grep -E "Intel Corporation UHD" <<< ${gpu_type}; then
+#if grep -E "NVIDIA|GeForce" <<< ${gpu_type}; then
+#    pacman -S nvidia --noconfirm --needed
+#	nvidia-xconfig
+#elif lspci | grep 'VGA' | grep -E "Radeon|AMD"; then
+#    pacman -S xf86-video-amdgpu --noconfirm --needed
+#elif grep -E "Integrated Graphics Controller" <<< ${gpu_type}; then
+#    pacman -S libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa --needed --noconfirm
+#elif grep -E "Intel Corporation UHD" <<< ${gpu_type}; then
     pacman -S libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa --needed --noconfirm
 fi
 #SETUP IS WRONG THIS IS RUN
