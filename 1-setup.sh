329472d8872861c906c2d702c37944d02e94b2db
exothermic88 <92395170+exothermic88@users.noreply.github.com>
2022-01-21 12:27:00 -0500

    Update 1-setup.sh

--- 1-setup.sh
+++ 1-setup.sh
@@ -99,17 +99,17 @@ echo -ne "
 -------------------------------------------------------------------------
 "
 # Graphics Drivers find and install
-gpu_type=$(lspci)
-if grep -E "NVIDIA|GeForce" <<< ${gpu_type}; then
-    pacman -S nvidia --noconfirm --needed
-	nvidia-xconfig
-elif lspci | grep 'VGA' | grep -E "Radeon|AMD"; then
-    pacman -S xf86-video-amdgpu --noconfirm --needed
-elif grep -E "Integrated Graphics Controller" <<< ${gpu_type}; then
+#gpu_type=$(lspci)
+#if grep -E "NVIDIA|GeForce" <<< ${gpu_type}; then
+#    pacman -S nvidia --noconfirm --needed
+#	nvidia-xconfig
+#elif lspci | grep 'VGA' | grep -E "Radeon|AMD"; then
+#    pacman -S xf86-video-amdgpu --noconfirm --needed
+#elif grep -E "Integrated Graphics Controller" <<< ${gpu_type}; then
+#    pacman -S libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa --needed --noconfirm
+#elif grep -E "Intel Corporation UHD" <<< ${gpu_type}; then
     pacman -S libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa --needed --noconfirm
-elif grep -E "Intel Corporation UHD" <<< ${gpu_type}; then
-    pacman -S libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa --needed --noconfirm
-fi
+#fi
 #SETUP IS WRONG THIS IS RUN
 if ! source /root/NCOS/setup.conf; then
 	# Loop through user input until the user gives a valid username
