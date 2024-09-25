f378278540769391ddcde4e87e8e44ec1328aeee
exothermic88 <nicolasciampa2001@gmail.com>
2024-09-25 10:31:34 -0400

    terminal + splash update

--- ncos/releng/airootfs/etc/skel/.config/fish/config.fish
+++ ncos/releng/airootfs/etc/skel/.config/fish/config.fish
@@ -2,7 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-fastfetch --logo /home/$USER/.config/fastfetch/fastfetch_logo -c paleofetch.jsonc
+#fastfetch --logo /home/$USER/.config/fastfetch/fastfetch_logo -c paleofetch.jsonc
+fastfetch
 
 alias ls='eza -al --color=always --group-directories-first' # my preferred listing
 alias la='eza -a --color=always --group-directories-first'  # all files and dirs
@@ -14,5 +15,26 @@ alias l...='eza -al --color=always --group-directories-first ../../../' # ls on 
 
 alias grep='grep --color=auto'
 
+function sp
+     subl $(fzf --preview 'bat --theme="Visual Studio Dark+" --color=always {}')
+end
+
+function codep
+     code $(fzf --preview 'bat --theme="Visual Studio Dark+" --color=always {}')
+end
+
+
+function gpt
+    set user_input $argv
+    set quoted_input "\"$user_input\""
+    tgpt $quoted_input
+end
+
+function gptm
+     tgpt -m
+end
+
 starship init fish | source
 
+
+thefuck --alias | source
